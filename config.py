fc50c946337faa308c328cc0e38a7eedb44b2c9d
Egor062016 <107456474+Egor062016@users.noreply.github.com>
2023-03-04 13:07:24 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+botik = '5833753029:AAGX_hH-dLVOqT3Kl7VYiuGJajRNBK0gEZE' #токен бота, в @botfather надо получить его
+contact_support = 'https://t.me/ElonRus' #контакт поддержки, контакт должен быть в виде ссылки
+admin_id = '5509031238' #id админа, можно получить в @getmyid_bot
+btc_wallet = 'типо кошелек' #btc кошелек сеть BEP20
+usdt_wallet = 'типо кошелек' #usdt кошелек сеть TRC20
+proccent = float('3')  #процент прибыли клиента при обмене
